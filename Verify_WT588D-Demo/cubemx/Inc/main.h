4767efce304924f307cd49baa9472a7af16fec35
Mr.Nights <2863692760@qq.com>
2025-09-18 15:55:15 +0800

    2025-09-18

---
+++ Verify_WT588D-Demo/cubemx/Inc/main.h
@@ -0,0 +1,83 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f1xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "bsp_sys.h"
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+extern UART_HandleTypeDef huart1;
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+void SystemClock_Config(void);
+void MX_GPIO_Init(void);
+void MX_USART1_UART_Init(void);
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define WT588D_BUSY_Pin GPIO_PIN_5
+#define WT588D_BUSY_GPIO_Port GPIOA
+#define VOICE_CTRL_Pin GPIO_PIN_6
+#define VOICE_CTRL_GPIO_Port GPIOA
+#define WT588D_RST_Pin GPIO_PIN_7
+#define WT588D_RST_GPIO_Port GPIOA
+#define WT588D_CLK_Pin GPIO_PIN_13
+#define WT588D_CLK_GPIO_Port GPIOB
+#define WT588D_DATA_Pin GPIO_PIN_15
+#define WT588D_DATA_GPIO_Port GPIOB
+#define WT588D_CS_Pin GPIO_PIN_6
+#define WT588D_CS_GPIO_Port GPIOC
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
